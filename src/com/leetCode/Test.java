17b09d78924c6953e8308ab2a1f1d354e6aced80
unknown <814466057@qq.com>
2020-09-30 10:58:35 +0800

    9-30题目

--- src/com/leetCode/Test.java
+++ src/com/leetCode/Test.java
@@ -5,8 +5,21 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        System.out.println(list);
+//        List<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        System.out.println(list);
+        int n = 10;
+        int arr[] = new int[11];
+
+        for(int i = 1;i <= n;i ++){
+            int j = i;
+            while(j <= n){
+                arr[j] += 1;
+                j = j + i;
+            }
+        }
+        for(int i=1;i <=n;i ++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }
