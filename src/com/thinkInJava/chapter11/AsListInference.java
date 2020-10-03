23c8457db30c7a58196e0f55060ab235ce28e2c2
unknown <814466057@qq.com>
2020-10-03 22:08:37 +0800

    11

--- src/com/thinkInJava/chapter11/AsListInference.java
+++ src/com/thinkInJava/chapter11/AsListInference.java
@@ -28,7 +28,7 @@ public class AsListInference {
         List<Snow> snow1 = Arrays.asList(new Cursty(),new Slush(),new Power());
         //List<Snow> snow2 = Arrays.asList(new Light(),new Heavy());会报错，因为Arrays.asList()中只有Power类型
         //因此会创建一个Power类型
-        List<Snow> snow3 = new ArrayList<Snow>();
+        List<Snow> snow3 = new ArrayList<>();
         Collections.addAll(snow3,new Light(),new Heavy());
         List<Snow> snow4 = Arrays.<Snow>asList(new Light(),new Heavy());    //这样可以
     }
