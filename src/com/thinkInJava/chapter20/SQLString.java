e872faf19baf95fbf019ad991627fe56e6ebc44b
unknown <814466057@qq.com>
2020-11-27 22:34:18 +0800

    11-27

--- src/com/thinkInJava/chapter20/SQLString.java
+++ src/com/thinkInJava/chapter20/SQLString.java
@@ -8,7 +8,9 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SQLString {
-    int value() default 0;
+    int value() default 0;  // 该元素用来规定数据库应该给被注解的变量设置多少大小
     String name() default "";
+    //注解嵌套，constraints元素的默认值是@Constraints,实际上是一个所有元素都为默认值的
+    //@Constraints值，因为没有加参数，
     Constraints constraints() default @Constraints;
 }
