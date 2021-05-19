13e58500b6abce382d4f68d442d504f67c3cc040
EEEEEEcho <814466057@qq.com>
2021-05-19 23:03:22 +0800

    05-19

--- src/com/leetCode/EveryDay/TwoSum.java
+++ src/com/leetCode/EveryDay/TwoSum.java
@@ -2,6 +2,7 @@ package com.leetCode.EveryDay;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
@@ -13,5 +14,16 @@ public class TwoSum {
             hashMap.put(target - nums[i],i);
         }
         return null;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
     }
 }
