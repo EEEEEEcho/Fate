e66f1e0803e6392468de35e542b7d03f837acdc2
EEEEEEcho <814466057@qq.com>
2021-05-26 20:50:00 +0800

    05-26

---
+++ src/com/leetCode/TreeTrain/PreorderTraversal.java
@@ -0,0 +1,21 @@
+package com.leetCode.TreeTrain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreorderTraversal {
+    List<Integer> result;
+    public List<Integer> preorderTraversal(TreeNode root) {
+        result = new ArrayList<>();
+        preOrderTravel(root);
+        return result;
+    }
+    public void preOrderTravel(TreeNode node){
+        if (node == null){
+            return;
+        }
+        result.add(node.val);
+        preOrderTravel(node.left);
+        preOrderTravel(node.right);
+    }
+}
