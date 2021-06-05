c5db9b8fbca67d31a07ed676b7e146ee6eb27596
EEEEEEcho <814466057@qq.com>
2021-06-05 19:49:23 +0800

    06-5

---
+++ src/com/leetCode/ArrayTrain/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+package com.leetCode.ArrayTrain;
+
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        int p = 0;
+        int q = p + 1;
+        while (q < nums.length){
+            if (nums[p] == nums[q]){
+                q ++;
+            }
+            else {
+                p ++;
+                nums[p] = nums[q];
+            }
+        }
+        return p;
+    }
+}
---
+++ src/com/leetCode/TreeTrain/IsCousins.java
@@ -0,0 +1,39 @@
+package com.leetCode.TreeTrain;
+
+import java.util.*;
+
+public class IsCousins {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int count = 0;
+        int nextCount = 1;  //记录是否遍历完一层
+        Map<Integer,Integer> hashMap = new HashMap<>();
+        hashMap.put(root.val, root.val);
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            count ++;
+            if(node.left != null){
+
+                hashMap.put(node.left.val,node.val);
+                queue.offer(node.left);
+            }
+            if (node.right != null){
+                hashMap.put(node.right.val,node.val);
+                queue.offer(node.right);
+            }
+            if(count == nextCount){
+                //遍历完了一层之后，在该层中找有没有堂兄弟
+                nextCount = queue.size();
+                count = 0;
+                Integer xPre = hashMap.get(x);
+                Integer yPre = hashMap.get(y);
+                if (xPre != null && !xPre.equals(yPre)){
+                    return true;
+                }
+                hashMap.clear();
+            }
+        }
+        return false;
+    }
+}
