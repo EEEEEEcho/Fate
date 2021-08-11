5162340a437009d9bf89c3d656268495840b13f6
EEEEEEcho <814466057@qq.com>
2021-08-11 22:43:47 +0800

    8-11

--- src/com/code/ArrayTrain/ShortestToChar.java
+++ src/com/code/ArrayTrain/ShortestToChar.java
@@ -1,7 +1,34 @@
 package com.code.ArrayTrain;
 
+import java.util.*;
+
 public class ShortestToChar {
     public int[] shortestToChar(String s, char c) {
-        return null;
+        List<Integer> cIndex = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (c == s.charAt(i)){
+                cIndex.add(i);
+            }
+        }
+        int[] result = new int[s.length()];
+        int start = 0;
+
+        for (int i = 0;i < s.length();i ++){
+            if (i < cIndex.get(0)){
+                result[i] = Math.abs(cIndex.get(0) - i);
+            }
+            else if (i > cIndex.get(cIndex.size() - 1)){
+                result[i] = Math.abs(i - cIndex.get(cIndex.size() - 1));
+            }
+            else{
+
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ShortestToChar s = new ShortestToChar();
+        System.out.println(Arrays.toString(s.shortestToChar("abba",'b')));
     }
 }
